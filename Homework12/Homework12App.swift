2a95323a9cc9af1a89d19145ef796c48e7a59e95
Akbota <omarbek.akbota@gmail.com>
2023-08-09 10:20:41 +0600

    Initial Commit

---
+++ Homework12/Homework12App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework12App.swift
+//  Homework12
+//
+//  Created by Akbota Omarbek on 09.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Homework12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
